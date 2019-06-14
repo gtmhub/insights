dadb79bf83fb8ec2d30158e2f33b74dec07fef24
Ivan Osmak <ivan@gtmhub.com>
2019-06-14 18:32:26 +0300

    Implemented "Closed epics" insight for Jira

---
+++ jira/closed_epics/algorithm.sql
@@ -0,0 +1,15 @@
+SELECT COUNT(*) as closed_epics
+FROM jira_issues
+WHERE
+    (
+        CASE WHEN NULLIF('%%jira_project%%', '') IS NOT NULL
+            THEN projectkey IN (SELECT(UNNEST(string_to_array('%%jira_project%%', ','))))
+            ELSE TRUE
+        END
+    )
+    AND
+        lower(issuetypename) = 'epic' 
+    AND 
+        NULLIF(resolutionname, '') IS NOT NULL 
+    AND
+        resolutiondate @@ '%%date_range%%'
