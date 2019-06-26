ddf1b9e5e571639d0f39e8c8a2a2bc3ff680a9eb
Ivan Osmak <ivan@gtmhub.com>
2019-06-26 17:10:14 +0300

    Implemented time to first resolution zendesk insight

---
+++ gallery/zendesk/tickets-avg-frt/algorithm.sql
@@ -0,0 +1,9 @@
+SELECT
+  ROUND(AVG(tm.first_resolution_time_in_minutes_business), 2) as avg_bus,
+  ROUND(AVG(tm.first_resolution_time_in_minutes_calendar), 2) as avg_cal
+FROM ticket_metrics tm
+  INNER JOIN (
+               SELECT id, created_at
+               FROM tickets) t
+    ON (tm.ticket_id = t.id)
+WHERE t.created_at @@ '%%date_range%%'
