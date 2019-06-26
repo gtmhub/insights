666a10a1867e3804ffdddddd0b7e2b1added0767
Ivan Osmak <ivan@gtmhub.com>
2019-06-26 17:28:50 +0300

    Implemented weekly velocity for Jira insight

---
+++ gallery/jira/weekly-velocity/algorithm.sql
@@ -0,0 +1,11 @@
+SELECT COALESCE(ROUND(AVG("count"), 2), 0) as weekly_velocity
+FROM (
+       SELECT
+         date_trunc('week', resolutiondate) as week,
+         COUNT(DISTINCT id) as "count"
+       FROM jira_issues
+       WHERE lower(resolutionname) = 'done' AND resolutiondate @@ '%%date_range%%'
+             AND lower(issuetypename) IN ('story', 'bug')
+       GROUP BY 1
+       ORDER BY 1 DESC
+     ) weeks
